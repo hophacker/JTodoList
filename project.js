f02c865144b4b7e017c01951425d324bce60d790
Jie Feng <jiefeng.hopkins@gmail.com>
2014-10-19 02:20:50 -0400

    ook

--- project.js
+++ project.js
@@ -1,10 +1,22 @@
 /*global Firebase: true */
 /*
-project {
-    state:  [0:newly created, 1:done, 2:archieved]
+   project {
+state:  [0:newly created, 1:done, 2:archieved]
 }
 */
+var importances = [
+    {level:0, name:'Low'     },
+    {level:1, name:'Medium'  },
+    {level:2, name:'High',   },
+    {level:3, name:'Extreme' }
+],  default_importance = importances[1];
+
 angular.module('project', ['ngRoute', 'firebase'])
+
+.config( [ '$compileProvider', function( $compileProvider ) {   
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+    // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+}])
 
 .value('fbURL', 'https://boiling-heat-52.firebaseio.com/')
 
@@ -33,19 +45,19 @@ angular.module('project', ['ngRoute', 'firebase'])
 .controller('ListCtrl', function($scope, $Projects) {
     $scope.projects = $Projects;
     $scope.archive = function() {
-      var oldProjects = $scope.projects;
-      $scope.projects = [];
-      angular.forEach(oldProjects, function(project) {
-        if (!project.done) $scope.projects.push(project);
-      });
+        var oldProjects = $scope.projects;
+        $scope.projects = [];
+        angular.forEach(oldProjects, function(project) {
+            if (!project.done) $scope.projects.push(project);
+        });
     };
 
     $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.projects, function(project) {
-        count += project.done ? 0 : 1;
-      });
-      return count;
+        var count = 0;
+        angular.forEach($scope.projects, function(project) {
+            count += project.done ? 0 : 1;
+        });
+        return count;
     };
     $scope.updateDone = function(project){
         console.log($Projects);
@@ -57,14 +69,8 @@ angular.module('project', ['ngRoute', 'firebase'])
 })
 
 .controller('CreateCtrl', function($scope, $location, $timeout, $Projects) {
-    $scope.importances = [
-        {name:'Extreme',    level:4},
-        {name:'High',       level:3},
-        {name:'Medium',     level:2},
-        {name:'Low',        level:1}
-    ];
-
-    $scope.project = {importance: $scope.importances[2]};
+    $scope.importances = importances;
+    $scope.project = {importance: default_importance};
 
     $scope.save = function() {
         console.log($Projects);
@@ -75,24 +81,39 @@ angular.module('project', ['ngRoute', 'firebase'])
     };
 })
 
-.controller('EditCtrl',
-            function($scope, $location, $routeParams, $Projects) {
-                var projectId = $routeParams.projectId,
-                projectIndex;
+.controller('EditCtrl', function($scope, $location, $routeParams, $Projects) {
+    var projectId = $routeParams.projectId,
+    projectIndex;
 
-                $scope.projects = $Projects;
-                projectIndex = $scope.projects.$indexFor(projectId);
-                $scope.project = $scope.projects[projectIndex];
+    $scope.importances = importances;
+    $scope.project = {importance: default_importance};
 
-                $scope.destroy = function() {
-                    $scope.projects.$remove($scope.project).then(function(data) {
-                        $location.path('/');
-                    });
-                };
+    $scope.projects = $Projects;
+    projectIndex = $scope.projects.$indexFor(projectId);
+    $scope.project = $scope.projects[projectIndex];
+    var level = $scope.project.importance.level;
+    $scope.project.importance = importances[level];
 
-                $scope.save = function() {
-                    $scope.projects.$save($scope.project).then(function(data) {
-                        $location.path('/');
-                    });
-                };
-            });
+
+    $scope.destroy = function() {
+        $scope.projects.$remove($scope.project).then(function(data) {
+            $location.path('/');
+        });
+    };
+
+    $scope.save = function() {
+        $scope.projects.$save($scope.project).then(function(data) {
+            $location.path('/');
+        });
+    };
+});
+
+/*.factory("simpleLogin", ["$firebaseSimpleLogin", 'fbURL', function($firebaseSimpleLogin, fbURL) {
+  console.log(fbURL);
+  var ref = new Firebase(fbURL);
+  return $firebaseSimpleLogin(ref);
+  }])
+// and use it in our controller
+.controller("SampleCtrl", ["$scope", "simpleLogin", function($scope, simpleLogin) {
+$scope.auth = simpleLogin;
+}]);*/
