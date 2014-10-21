9f619ed51fc5b0538b95277606373f35e8bacf8a
Jie Feng <jiefeng.hopkins@gmail.com>
2014-10-20 23:25:23 -0400

    hello

--- project.js
+++ project.js
@@ -1,119 +1,168 @@
 /*global Firebase: true */
 /*
-   project {
-state:  [0:newly created, 1:done, 2:archieved]
-}
-*/
+ project {
+ state:  [0:newly created, 1:done, 2:archieved]
+ }
+ */
 var importances = [
     {level:0, name:'Low'     },
     {level:1, name:'Medium'  },
-    {level:2, name:'High',   },
+    {level:2, name:'High'    },
     {level:3, name:'Extreme' }
 ],  default_importance = importances[1];
+var firebaseURL = 'https://boiling-heat-52.firebaseio.com/',
+    username = 'hophacker/',
+    base = firebaseURL + username;
 
 angular.module('project', ['ngRoute', 'firebase'])
 
-.config( [ '$compileProvider', function( $compileProvider ) {   
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
-    // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
-}])
+    .config( [ '$compileProvider', function( $compileProvider ) {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }])
 
-.value('fbURL', 'https://boiling-heat-52.firebaseio.com/')
+    .value('URL', {
+        base: base,
+        todo: base + 'todo/',
+        finished: base + 'finished/'
+    })
 
-.factory('$Projects', function($firebase, fbURL) {
-    return $firebase(new Firebase(fbURL)).$asArray();
-})
-.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        controller:'ListCtrl',
-        templateUrl:'list.html'
+    .factory('$TODO', function($firebase, URL) {
+        return $firebase(new Firebase(URL.todo))
     })
-    .when('/edit/:projectId', {
-        controller:'EditCtrl',
-        templateUrl:'detail.html'
+    .factory('$FIN', function($firebase, URL) {
+        return $firebase(new Firebase(URL.finished))
     })
-    .when('/new', {
-        controller:'CreateCtrl',
-        templateUrl:'detail.html'
+    .factory('$TODOArray', function($firebase, URL) {
+        return $firebase(new Firebase(URL.todo)).$asArray()
     })
-    .otherwise({
-        redirectTo:'/'
+    .factory('$FINArray', function($firebase, URL) {
+        return $firebase(new Firebase(URL.finished)).$asArray()
+    })
+    .factory('$fbPatch', function(){
+        this.clearObj = function(obj){
+            delete obj.$id
+            delete obj.$priority
+            delete obj.$$hashKey
+            return obj;
+        }
+        return this;
+    })
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller:'ListCtrl',
+                templateUrl:'list.html'
+            })
+            .when('/edit/:showType/:projectId', {
+                controller:'EditCtrl',
+                templateUrl:'detail.html'
+            })
+            .when('/new', {
+                controller:'CreateCtrl',
+                templateUrl:'detail.html'
+            })
+            .otherwise({
+                redirectTo:'/'
+            });
+    })
+
+    .controller('ListCtrl', function($scope, $TODOArray, $FINArray, $fbPatch) {
+        $scope.projects = $TODOArray;
+        $scope.showType = "TODO";
+        $scope.archive = function() {
+            angular.forEach($scope.projects, function(project, key) {
+                if (project.done){
+                    $scope.projects.$remove(key).then(function(data){
+                        $FINArray.$add($fbPatch.clearObj(project)).then(function(data) {
+                        });
+                    })
+                }
+            });
+        };
+
+
+        $scope.showFinished = function(){
+            $scope.projects = $FINArray;
+            $scope.showType = "FIN";
+        }
+
+        $scope.showTodo = function(){
+            $scope.projects = $TODOArray;
+            $scope.showType = "TODO";
+        }
+
+        $scope.remaining = function() {
+            var count = 0;
+            angular.forEach($scope.projects, function(project) {
+                count += project.done ? 0 : 1;
+            });
+            return count;
+        };
+        $scope.updateDone = function(project){
+//            var item = $TODO.$getRecord( project.$id );
+//            item.done = project.done;
+//            $TODO.$save(item);
+            //$Pro.update(project.$id, {done: project.done});
+        };
+    })
+
+    .controller('CreateCtrl', function($scope, $location, $timeout, $TODOArray) {
+        $scope.importances = importances;
+        $scope.project = {importance: default_importance};
+
+        $scope.save = function() {
+            $scope.project.done = false;
+            $TODOArray.$add($scope.project).then(function(data) {
+                $location.path('/');
+            });
+        };
+    })
+
+    .controller('EditCtrl', function($scope, $location, $routeParams, $TODOArray, $FINArray) {
+        var projectId = $routeParams.projectId,
+            showType = $routeParams.showType,
+            projectIndex;
+
+        switch(showType){
+            case "TODO":
+                $scope.projects = $TODOArray;
+                break;
+            case "FIN":
+                $scope.projects = $FINArray;
+                break;
+            default:
+        }
+        $scope.importances = importances;
+
+        if (projectId === null){
+            $scope.project = {importance: default_importance};
+        }else{
+            projectIndex = $scope.projects.$indexFor(projectId);
+            $scope.project = $scope.projects[projectIndex];
+            var level = $scope.project.importance.level;
+            $scope.project.importance = importances[level];
+        }
+
+        $scope.destroy = function() {
+            $scope.projects.$remove($scope.project).then(function(data) {
+                $location.path('/');
+            });
+        };
+
+        $scope.save = function() {
+            $scope.projects.$save($scope.project).then(function(data) {
+                $location.path('/');
+            });
+        };
     });
-})
 
-.controller('ListCtrl', function($scope, $Projects) {
-    $scope.projects = $Projects;
-    $scope.archive = function() {
-        var oldProjects = $scope.projects;
-        $scope.projects = [];
-        angular.forEach(oldProjects, function(project) {
-            if (!project.done) $scope.projects.push(project);
-        });
-    };
-
-    $scope.remaining = function() {
-        var count = 0;
-        angular.forEach($scope.projects, function(project) {
-            count += project.done ? 0 : 1;
-        });
-        return count;
-    };
-    $scope.updateDone = function(project){
-        console.log($Projects);
-        var item = $Projects.$getRecord( project.$id );
-        item.done = project.done;
-        $Projects.$save(item);
-        //$Pro.update(project.$id, {done: project.done});
-    };
-})
-
-.controller('CreateCtrl', function($scope, $location, $timeout, $Projects) {
-    $scope.importances = importances;
-    $scope.project = {importance: default_importance};
-
-    $scope.save = function() {
-        console.log($Projects);
-        $scope.project.done = false;
-        $Projects.$add($scope.project).then(function(data) {
-            $location.path('/');
-        });
-    };
-})
-
-.controller('EditCtrl', function($scope, $location, $routeParams, $Projects) {
-    var projectId = $routeParams.projectId,
-    projectIndex;
-
-    $scope.importances = importances;
-    $scope.project = {importance: default_importance};
-
-    $scope.projects = $Projects;
-    projectIndex = $scope.projects.$indexFor(projectId);
-    $scope.project = $scope.projects[projectIndex];
-    var level = $scope.project.importance.level;
-    $scope.project.importance = importances[level];
-
-
-    $scope.destroy = function() {
-        $scope.projects.$remove($scope.project).then(function(data) {
-            $location.path('/');
-        });
-    };
-
-    $scope.save = function() {
-        $scope.projects.$save($scope.project).then(function(data) {
-            $location.path('/');
-        });
-    };
-});
-
-/*.factory("simpleLogin", ["$firebaseSimpleLogin", 'fbURL', function($firebaseSimpleLogin, fbURL) {
-  console.log(fbURL);
-  var ref = new Firebase(fbURL);
-  return $firebaseSimpleLogin(ref);
-  }])
-// and use it in our controller
-.controller("SampleCtrl", ["$scope", "simpleLogin", function($scope, simpleLogin) {
-$scope.auth = simpleLogin;
-}]);*/
+/*.factory("simpleLogin", ["$firebaseSimpleLogin", 'URL', function($firebaseSimpleLogin, URL) {
+ console.log(URL);
+ var ref = new Firebase(URL);
+ return $firebaseSimpleLogin(ref);
+ }])
+ // and use it in our controller
+ .controller("SampleCtrl", ["$scope", "simpleLogin", function($scope, simpleLogin) {
+ $scope.auth = simpleLogin;
+ }]);*/
