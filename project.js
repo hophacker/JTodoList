b074e464f7e8dd8091483166648907263c1a6609
Jie Feng <jiefeng.hopkins@gmail.com>
2014-10-25 18:58:53 -0400

    new design

--- project.js
+++ project.js
@@ -4,16 +4,16 @@ See the file license.txt for copying permission.*/
 /*global Firebase: true */
 /*
  project {
- state:  [0:newly created, 1:done, 2:archieved]
+ state:  [0:newly created, 1:done, 2:finhieved]
  }
  */
 var port = chrome.extension.connect({name: "StorageConnection"});
 
 var importances = [
-    {level:0, name:'Low'     },
-    {level:1, name:'Medium'  },
-    {level:2, name:'High'    },
-    {level:3, name:'Extreme' }],
+    {value:0, text:'Low'     },
+    {value:1, text:'Medium'  },
+    {value:2, text:'High'    },
+    {value:3, text:'Extreme' }],
     default_importance = importances[1];
 
 function checkSignin(ifsigin){
@@ -31,7 +31,10 @@ function gotoUrl($location, $scope, url){
     if(!$scope.$$phase) $scope.$apply(); //important!!!
 }
 
-var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
+var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap', 'xeditable'])
+    .run(function(editableOptions) {
+        editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+    })
     .config(function($routeProvider) {
         $routeProvider
             .when('/list', {
@@ -47,7 +50,7 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
                 controller: 'signupCtrl',
                 templateUrl:'login.html'
             })
-            .when('/edit/:showType/:projectId', {
+            .when('/edit/:type/:projectId', {
                 controller:'EditCtrl',
                 templateUrl:'detail.html'
             })
@@ -81,15 +84,33 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
             $scope =  $s;
         };
         $PROJECT.prototype = {
-            archive: function (id) {
+            getSource: function(type){
+                switch(type){
+                    case 'todo':
+                        return $scope.todoProjects;
+                    case 'finished':
+                        return $scope.finishedProjects;
+                    default:;
+                }
+            },
+            finish: function (id) {
                 var p = $URL.$TODOArray().$getRecord(id);
                 $scope.todoProjects.$remove(p).then(function(){
-                    $scope.archivedProjects.$add(p);
+                    $scope.finishedProjects.$add(p);
                 })
             },
-            removeTodo: function(id){
-                var p = $URL.$TODOArray().$getRecord(id);
-                $scope.todoProjects.$remove(p);
+            remove: function(type, id){
+                switch(type){
+                    case 'todo':
+                        var p = $URL.$TODOArray().$getRecord(id);
+                        $scope.todoProjects.$remove(p);
+                        break;
+                    case 'finished':
+                        var p = $URL.$FINArray().$getRecord(id);
+                        $scope.finishedProjects.$remove(p);
+                        break;
+                    default:;
+                }
             }
         }
         return $PROJECT;
@@ -101,7 +122,7 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
             todo        : '',
             finished    : '',
             todoArray   : null,
-            arcArray    : null,
+            finArray    : null,
             setUID      : function(UID){
                 console.log("setUID");
                 this.base = this.firebaseURL + 'users/' + UID + "/";
@@ -112,13 +133,26 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
                 if (this.todoArray === null) this.todoArray = $firebase(new Firebase(this.todo)).$asArray();
                 return this.todoArray;
             },
-            $ARCArray   : function(){
-                if (this.arcArray === null) this.arcArray = $firebase(new Firebase(this.finished)).$asArray();
-                return this.arcArray;
+            $FINArray   : function(){
+                if (this.finArray === null) this.finArray = $firebase(new Firebase(this.finished)).$asArray();
+                return this.finArray;
             },
             $firebaseRef: function(){
                 console.log(this.firebaseURL);
                 return new Firebase(this.firebaseURL);
+            },
+            getSource   : function(type){
+                switch(type){
+                    case 'todo':
+                        return this.todoArray;
+                        break;
+                    case 'finished':
+                        return this.finArray;
+                        break;
+                    default:;
+
+                }
+
             }
         };
     })
@@ -239,30 +273,43 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
     })
     .controller('ListCtrl', function($scope, $URL, $sharedService, $PROJECT) {
         var $project = new $PROJECT($scope);
-
-        $TODO =$URL.$TODOArray();
-        $scope.todoProjects =  $TODO;
-        $scope.archivedProjects = $URL.$ARCArray();
+        $scope.importances = importances;
+        $scope.todoProjects = $URL.$TODOArray();
+        $scope.finishedProjects = $URL.$FINArray();
         $scope.showType = "TODO";
+        $scope.addProject = null;
         $scope.$on('setFilterWord', function() {
             $scope.search = $sharedService.filterWord;
         });
-        $scope.archiveAll = function() {
-            var archivedIds = [];
+        $scope.finishAll = function() {
+            var finishedIds = [];
             angular.forEach($scope.todoProjects, function(todoP, key) {
                 if (todoP.done)
-                    archivedIds.push(todoP.$id);
+                    finishedIds.push(todoP.$id);
             });
-            angular.forEach(archivedIds, function(id, key) {
-                $project.archive(id);
+            angular.forEach(finishedIds, function(id, key) {
+                $project.finish(id);
             });
         };
-        $scope.archive = function(id){
-            $project.archive(id);
+        $scope.finish = function(id){
+            $project.finish(id);
         }
-        $scope.remove = function(id){
-            $project.removeTodo(id);
-        }
+
+        $scope.save = function(type, project, $data) {
+            project.description = $data.description;
+            project.importance = $data.importance;
+            console.log(project);
+            var projects = $project.getSource(type);
+            projects.$save(project);
+//            if (project.$id) projects.$save(project);
+//            else projects.$save($data);
+        };
+        $scope.addTodoProject = function() {
+            $scope.todoProjects.$add($scope.addProject);
+            $scope.addProject = {};
+        };
+
+
         $scope.remaining = function() {
             var count = 0;
             angular.forEach($scope.todoProjects, function(project) {
@@ -292,29 +339,25 @@ var app = angular.module('project', ['ngRoute', 'firebase', 'ui.bootstrap'])
 
     .controller('EditCtrl', function($scope, $location, $routeParams, $URL) {
         var projectId = $routeParams.projectId,
-            $TODO = $URL.$TODOArray();
+            $SOURCE = $URL.getSource($routeParams.type)
+
 
         $scope.importances = importances;
 
         if (projectId === null){
             $scope.project = {importance: default_importance};
         }else{
-            $scope.project = $TODO.$getRecord(projectId);
+            $scope.project = $SOURCE.$getRecord(projectId);
             var level = $scope.project.importance.level;
             $scope.project.importance = importances[level];
         }
 
         $scope.destroy = function() {
-            $TODO.$remove($scope.project).then(function(data) {
+            $SOURCE.$remove($scope.project).then(function(data) {
                 $location.path('/list');
             });
         };
 
-        $scope.save = function() {
-            $TODO.$save($scope.project).then(function(data) {
-                $location.path('/list');
-            });
-        };
     });
 
 app.factory('$sharedService', function($rootScope){
@@ -374,3 +417,4 @@ app.directive('ngEnter', function () {
 //    });
 //});
 
+//            angular.extend(project, {'$id': $id})
